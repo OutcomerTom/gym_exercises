df76f6ebad81b86d49d59cfb23bfd12e68fea1cc
OutcomerTom <tom@outcomer.com>
2023-08-24 12:50:11 +0200

    Create AppJS and modified gitignore file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import App from "./App";
+
 const root = ReactDom.createRoot(document.getElementById("root"));
+
+root.render(
+  <App />
+);
