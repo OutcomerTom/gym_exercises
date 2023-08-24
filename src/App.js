28b79dead83dbfcdd2c1c3f50887f1cc3ee269cd
OutcomerTom <tom@outcomer.com>
2023-08-24 16:32:13 +0200

    Created components folder with indexJS, Navbar and Footer files; created pages folder with indexJS, Home and ExerciseDetail files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Box } from "@mui/material";
+import { Home, ExerciseDetail } from "./pages";
+import { Navbar, Footer } from "./components";
 
 import "./App.css";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Box width="400px" sx={{ width: { xl: "1488px" } }} m="auto">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
+      </Routes>
+      <Footer />
+    </Box>
+  );
 };
 
 export default App;
